e05f942072654b237d95cbdd4033fe14423d8e1d
Vinicius-O-Ferraz <vinicius.f.ferraz.bsi@gmail.com>
2024-09-15 14:24:38 -0300

    initial build

---
+++ src/Models.py
@@ -0,0 +1,3 @@
+"""Responsável por lidar com a lógica de negócios do aplicativo e acessar os dados. Ela pode ser implementada usando uma biblioteca de ORM,
+como SQLAlchemy ou Tortoise ORM. Com o ORM, você pode definir os seus modelos de banco de dados como classes Python e, em seguida, interagir
+com o banco de dados usando essas classes."""
